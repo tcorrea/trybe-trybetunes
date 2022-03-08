1dd248baa4a2c8829da597f692504491333d3690
Thiago Corrêa <thiagocorreacontato@gmail.com>
2022-03-08 11:59:32 -0300

    feat: requisito 5 - formulário de pesquisa

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchField: '',
+      disabled: true,
+    };
+  }
+
+  handleValidade = (event) => {
+    const { value } = event.target;
+    const MIN_CHAR_ALLOWED = 2;
+    if (value.length >= MIN_CHAR_ALLOWED) this.setState({ disabled: false });
+    this.setState({ searchField: value });
+  };
+
   render() {
+    const { searchField, disabled } = this.state;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          Search
+          <form>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleValidade }
+              value={ searchField }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ disabled }
+            >
+              Pesquisar
+            </button>
+          </form>
         </div>
       </>
     );
